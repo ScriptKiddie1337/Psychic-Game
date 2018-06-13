6935f18a0c8321f0946e4b2cd27bd759c827645f
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-06-12 21:31:33 -0700

    css updates and js improvements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,56 @@
-//Global Variables
-let winCount = 0 
-let loseCount = 0
-let guessCount = 9
-let winningNumberChoice = $("#guess-count");
-let letterChoices = ["a", "b", "c"]; //finish alphabet later
-//let guessCount = document.getElementById("guess-count")
+ //Global Variables
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z"];
 
-//Be able to click a key and generate a random number [1]//
-let userGuess = document.getElementById("user-guess");
-document.onkeyup = function(event) {
-  userGuess.textContent = event.key;
-  
-  renderGame(event.key);
-  
+var guessedLetters = [];
+
+var letterToGuess = null;
+
+var guessesLeft = 9;
+
+var wins = 0;
+var losses = 0;
+
+
+var updateGuessesLeft = function() {
+  document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+console.log(updateGuessesLeft)
+var updateLetterToGuess = function() {
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+console.log(updateLetterToGuess)
+var updateGuessesSoFar = function() {
+  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+console.log(updateGuessesSoFar)
+
+var reset = function() {
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
 };
 
+updateLetterToGuess();
+updateGuessesLeft();
 
+document.onkeydown = function(event) {
+  guessesLeft--;
+  var letter = String.fromCharCode(event.which).toLowerCase();
+  guessedLetters.push(letter);
 
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
-function renderGuess(userGuess) {
-  winningNumberChoice = letterChoices.charAt(Math.floor(Math.random() * letterChoices.length));
-  if (userGuess === winningNumberChoice) {
-    return winCount++;
-  } 
-  else if (userGuess !== winningNumberChoice) {
-    return guessCount--;
+  if (letter === letterToGuess) {
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
+    reset();
   }
-  else if (guessCount === 0) {
-    return loseCount++;
+  if (guessesLeft === 0) {
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+    reset();
   }
-  else if (winCount === 5) {
-    alert("You Are Psychic!")
-  }
-  else if (loseCount === 10) {
-    alert("maybe this isn't your shtick")
-  }
-}
-
-
-
-//Displays the Guesses - Up to 9 keys displayed - create a storage to save and keep pushing, like an array or an object, look at how to store data in an array
-
-
-//Counts wins and losses - similar to rock paper scissors logic
-
-//Counts Guesses - 9 Guesses then losses - similar model to counting wins and losses
-
-//When They Guess Correctly they Win
-
-//Create a winning Number and generate it randomly
+}; 
